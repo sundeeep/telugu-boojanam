c4575a80e5535e4c20905eacdeb441fde4ee99a6
sundeeep <sandeepkumar25820@gmail.com>
2024-10-09 00:10:39 +0530

    🐛FIX-Server: CORS Code got bugs, Fixed by new code.

--- server/src/app.js
+++ server/src/app.js
@@ -8,18 +8,25 @@ import cors from 'cors';
 
 const app = express();
 
-const whitelist = ['http://localhost:5173', 'https://telugubojanam.vercel.app'];
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-}
-
+const allowedOrigins = [
+    "http://localhost:5173",
+    "https://telugubojanam.vercel.app"
+  ];
+  
+  // CORS options
+   const corsOptions = {
+    origin: (origin, callback) => {
+      if (allowedOrigins.includes(origin) || !origin) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  };
+  
 app.use(cors(corsOptions));
+
 app.use(express.json({limit: "16kb"}));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
