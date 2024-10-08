fc7e0e577a56cde75996b6c6dcad4c3d06a476a8
sundeeep <sandeepkumar25820@gmail.com>
2024-10-08 23:56:03 +0530

    🚀Middlewares-Server: Common Middlewares are added to express app!

--- server/src/app.js
+++ server/src/app.js
@@ -20,14 +20,15 @@ const corsOptions = {
 }
 
 app.use(cors(corsOptions));
-
-app.use(express.json());
+app.use(express.json({limit: "16kb"}));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
 
 app.get("/", (req, res)=>{
     res.status(200).json({
         message: "Welcome to drugboard.ai API..."
     });
-})
+});
 
 const drugboardServer = http.createServer(app);
 
