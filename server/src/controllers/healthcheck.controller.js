d045c87c386ccb2e3826e3f4ca33fcc3dfdfd638
sundeeep <sandeepkumar25820@gmail.com>
2024-10-08 23:47:31 +0530

    🚀Controller-Server: API Health Check Controller is written!

---
+++ server/src/controllers/healthcheck.controller.js
@@ -0,0 +1,10 @@
+import APISuccessResponse from "../lib/APISuccessResponse";
+import asyncHandler from "../utils/asyncHandler";
+
+const healthCheck = asyncHandler(async(req, res, next)=>{
+    return res
+            .status(200)
+            .json(APISuccessResponse(200, "OK", "🚀 API Health Check has been passed! 💚"))
+})
+
+export default healthCheck;
