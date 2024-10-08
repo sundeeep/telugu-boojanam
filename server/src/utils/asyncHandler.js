702646043f4aca4fb5cd8c171b1aaf3b5735e41c
sundeeep <sandeepkumar25820@gmail.com>
2024-10-09 00:19:57 +0530

    🐛FIX-Server: req, res, next params added!

--- server/src/utils/asyncHandler.js
+++ server/src/utils/asyncHandler.js
@@ -1,5 +1,5 @@
 const asyncHandler = (requestHandler) => {
-    return () => {
+    return (req, res, next) => {
         Promise
             .resolve(requestHandler(req, res, next))
             .catch((error)=>next(error));
