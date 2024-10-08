958e94836c7d31935dc19b3db725885116776a05
sundeeep <sandeepkumar25820@gmail.com>
2024-10-09 00:02:10 +0530

    🚀Middleware-Server: Route is added to express app to serve all the routes.

--- server/src/app.js
+++ server/src/app.js
@@ -24,6 +24,14 @@ app.use(express.json({limit: "16kb"}));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 
+/**
+ * Import all the application Routes Here...
+ */
+
+import healthCheckRouter from "./routes/healthcheck.routes.js"
+app.use("/api/v1/healthcheck", healthCheckRouter);
+
+
 app.get("/", (req, res)=>{
     res.status(200).json({
         message: "Welcome to drugboard.ai API..."
